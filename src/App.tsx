023813d8717b9156d7ede4c6ca54066e1185cb1c
Paramjit Singh <web@paramjit.org>
2022-08-17 12:55:23 +0200

    init commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <p>
+        Work in progress...
+      </p>
+    </div>
+  )
+}
+
+export default App
